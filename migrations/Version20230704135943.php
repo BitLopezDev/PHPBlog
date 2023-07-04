7759af5d23848afe9d43c91c83eac26ceb1e5ce7
Santiago C <santiagocomesana03@gmail.com>
2023-07-04 11:29:59 -0300

    error

---
+++ migrations/Version20230704135943.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230704135943 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE post ADD avance VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE users CHANGE totp totp BIGINT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE post DROP avance');
+        $this->addSql('ALTER TABLE users CHANGE totp totp VARCHAR(255) DEFAULT NULL');
+    }
+}
